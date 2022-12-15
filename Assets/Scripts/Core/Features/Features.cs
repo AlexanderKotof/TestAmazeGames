638e0120456cfa5d91a98d3364c4c60e321af802
Alexandr Kotov <newbeedevinc@gmail.com>
2022-12-15 22:24:21 +0400

    Feature System added

---
+++ Assets/Scripts/Core/Features/Features.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FeatureSystem.Features
+{
+    public class Features : MonoBehaviour
+    {
+        public Feature[] features;
+
+        private static Features _instance;
+
+        private readonly Dictionary<Type, IFeature> _typesToFeature = new Dictionary<Type, IFeature>();
+
+        private void Awake()
+        {
+            DontDestroyOnLoad(this);
+
+            _instance = this;
+
+            foreach (var feature in features)
+            {
+                _typesToFeature.Add(feature.GetType(), feature);
+            }
+        }
+
+        public static Feature[] GetFeatures()
+        {
+            return _instance.features;
+        }
+
+        public static T GetFeature<T>() where T : IFeature
+        {
+            if (_instance._typesToFeature.TryGetValue(typeof(T), out var feature))
+                return (T)feature;
+
+            Debug.LogError($"No Feature of type {typeof(T).Name} was founded!");
+            return default;
+        }
+    }
+}
